13b974af70de6191d55654bcddb0dc176c319e6d
Ahmed Gamal <agamal0077@gmail.com>
2024-03-17 02:09:20 +0200

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 import state from '../store'
-
+import { CustomButton } from '../components'
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -26,6 +26,37 @@ const Home = () => {
               className="w-8 h-8 object-contain"
             ></img>
           </motion.header>
+
+          <motion.div
+            className="home-content"
+            {...headContainerAnimation}
+          >
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO
+                IT.
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md text-grey-600 text-base ">
+                create your uniqe and exclusive shirt with
+                our brand-new 3D customzation tool.{' '}
+                <strong>Unleash your imagination</strong>
+                {''} and define your own style.{' '}
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles=""
+              />
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
