8c4170b4486b5a8ed613a069c914cf23e9fea355
Ahmed Gamal <agamal0077@gmail.com>
2024-03-17 20:22:33 +0200

    Go back button

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -33,23 +33,37 @@ const Customizer = () => {
   return (
     <AnimatePresence>
       {!snap.intro && (
-        <motion.div
-          key="custom"
-          className="absolute top-0 left-0 "
-          {...slideAnimation('left')}
-        >
-          <div className="flex items-center min-h-screen">
-            <div className="editortabs-container tabs">
-              {EditorTabs.map((tab) => (
-                <Tab
-                  key={tab.name}
-                  tab={tab}
-                  handleClick={() => {}}
-                />
-              ))}
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 "
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
+
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="GO Back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+        </>
       )}
     </AnimatePresence>
   )
