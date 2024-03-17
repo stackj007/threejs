24d4dab2c5043dae4b11de12dd06613771d43a04
Ahmed Gamal <agamal0077@gmail.com>
2024-03-17 03:06:06 +0200

    conditionally render Customize page if snap.intro is false

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import config from '../config/config'
+import state from '../store'
+import { download } from '../assets'
+import {
+  downloadCanvasToImage,
+  reader,
+} from '../config/helpers'
+import {
+  EditorTabs,
+  FilterTabs,
+  DecalTypes,
+} from '../config/constants'
+import {
+  fadeAnimation,
+  slideAnimation,
+} from '../config/motion'
+
+import {
+  AiPicker,
+  FilePicker,
+  ColorPicker,
+  CustomButton,
+  Tab,
+} from '../components'
 
 const Customizer = () => {
-  return <div>Customizer</div>
+  const snap = useSnapshot(state)
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && <>CUstomize</>}
+    </AnimatePresence>
+  )
 }
 
 export default Customizer
